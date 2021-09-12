2b0b2272a93b334bba1d6966b15812d3777ad792
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-09-12 18:54:23 +0400

    builder complete

--- design-patterns/src/main/java/builder/RobotBuilder.java
+++ design-patterns/src/main/java/builder/RobotBuilder.java
@@ -1,4 +1,17 @@
 package builder;
 
-public class RobotBuilder {
+// Defines the methods needed for creating parts
+// for the robot
+public interface RobotBuilder {
+
+    public void buildRobotHead();
+
+    public void buildRobotTorso();
+
+    public void buildRobotArms();
+
+    public void buildRobotLegs();
+
+    public Robot getRobot();
+
 }
