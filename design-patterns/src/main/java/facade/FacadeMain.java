56ccf617e2f8b5345cc4139fbd1c258426f43fd5
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-09-14 13:42:49 +0400

    dacade completed

--- design-patterns/src/main/java/facade/FacadeMain.java
+++ design-patterns/src/main/java/facade/FacadeMain.java
@@ -3,7 +3,9 @@ package facade;
 public class FacadeMain {
 
     public static void main(String[] args) {
-
+        BankAccountFacade accessingBank = new BankAccountFacade(12345678, 1234);
+        accessingBank.withdrawCash(50.00);
+        accessingBank.withdrawCash(990.00);
     }
 
 }
