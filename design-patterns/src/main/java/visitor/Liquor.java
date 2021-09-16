53a3cb255828865f398d6f1fb9c4c41177e74606
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-09-16 18:06:31 +0400

    visitor complete

---
+++ design-patterns/src/main/java/visitor/Liquor.java
@@ -0,0 +1,20 @@
+package visitor;
+
+class Liquor implements Visitable {
+
+    private double price;
+
+    Liquor(double item) {
+        price = item;
+    }
+
+    @Override
+    public double accept(Visitor visitor) {
+        return visitor.visit(this);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
---
+++ design-patterns/src/main/java/visitor/Necessity.java
@@ -0,0 +1,20 @@
+package visitor;
+
+class Necessity implements Visitable {
+
+    private double price;
+
+    Necessity(double item) {
+        price = item;
+    }
+
+    @Override
+    public double accept(Visitor visitor) {
+        return visitor.visit(this);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
