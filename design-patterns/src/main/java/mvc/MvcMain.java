21cec86d92b7f2d07e4882ed10e7b0060dba1eb4
DennisKingsman <den.kuznecov.1999@mail.ru>
2021-09-16 18:12:48 +0400

    mvc complete

--- design-patterns/src/main/java/mvc/MvcMain.java
+++ design-patterns/src/main/java/mvc/MvcMain.java
@@ -3,7 +3,10 @@ package mvc;
 public class MvcMain {
 
     public static void main(String[] args) {
-
+        CalculatorView theView = new CalculatorView();
+        CalculatorModel theModel = new CalculatorModel();
+        CalculatorController theController = new CalculatorController(theView,theModel);
+        theView.setVisible(true);
     }
 
 }
